1ddc7d9b68e6c60cd5eee4f10646ea32fcbe4847
DavidZD02 <111716602+DavidZD02@users.noreply.github.com>
2022-11-20 15:28:18 -0500

    Add files via upload

---
+++ Parcial3_HPC/Debug/CMakeFiles/ParcialD.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/Documentos/HPC/ParcialD/Extraccion/extraction.cpp" "CMakeFiles/ParcialD.dir/Extraccion/extraction.cpp.o" "gcc" "CMakeFiles/ParcialD.dir/Extraccion/extraction.cpp.o.d"
+  "/home/ubuntu/Documentos/HPC/ParcialD/Regresion/linearregression.cpp" "CMakeFiles/ParcialD.dir/Regresion/linearregression.cpp.o" "gcc" "CMakeFiles/ParcialD.dir/Regresion/linearregression.cpp.o.d"
+  "/home/ubuntu/Documentos/HPC/ParcialD/main.cpp" "CMakeFiles/ParcialD.dir/main.cpp.o" "gcc" "CMakeFiles/ParcialD.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Parcial3_HPC/Debug/CMakeFiles/ParcialD.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ParcialD.dir/Extraccion/extraction.cpp.o"
+  "CMakeFiles/ParcialD.dir/Extraccion/extraction.cpp.o.d"
+  "CMakeFiles/ParcialD.dir/Regresion/linearregression.cpp.o"
+  "CMakeFiles/ParcialD.dir/Regresion/linearregression.cpp.o.d"
+  "CMakeFiles/ParcialD.dir/main.cpp.o"
+  "CMakeFiles/ParcialD.dir/main.cpp.o.d"
+  "ParcialD"
+  "ParcialD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ParcialD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
